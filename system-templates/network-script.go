c75cd2cc43bf27592ae72d64c81e94e5066e8ebe
Excellent <excellent.nomad@gmail.com>
2017-06-13 21:23:43 +0600

    Added backup feature of files

--- system-templates/network-script.go
+++ system-templates/network-script.go
@@ -39,50 +39,86 @@ type Config struct {
 } 
 
 func main() {
-	//const shortForm = "2006-Jan-02"
-
+	t := time.Now()
+	datetime := strings.Replace(strings.Split(t.String(), ".")[0], " ", "-", -1)
+	
+	//conf contains all the data contained in config.json file
 	conf := LoadConfig("./config.json")
-	t := template.Must(template.New("network.tpl").ParseFiles("network.tpl"))
+
+	// A new template is created and parsed before being used
+	tpl := template.Must(template.New("network.tpl").ParseFiles("network.tpl"))
 	
-	bakFile := []string{conf.Filepaths.Netinfo, "bak"}
-	bakFileAbsPath := strings.Join(bakFile, ".")
+	// filenames contains [etc network interfaces] after splitting /etc/network/interfaces
+	filenames := strings.Split(conf.Filepaths.Netinfo, "/")
+	// filename grabs last element from filenames, appends datetime and bak
+	filename  := []string{filenames[len(filenames) - 1], datetime, "bak"}
+	
+	bakDir  := []string{conf.Filepaths.Netinfo, "bak"}
+	//bakFile is a new file name for the backup file with dots between elements
+	bakFile := strings.Join(filename, ".")
+	
+	bakDirAbsPath  := strings.Join(bakDir, ".")
+	bakFileJoin := []string{bakDirAbsPath, bakFile}
+	bakFileAbsPath := strings.Join(bakFileJoin, "/")
+	
+	fmt.Println("bakFile:", bakFile)
+	fmt.Println("bakDirAbsPath:", bakDirAbsPath)
+
+	// create directory if it does not exits
+	if _, err := os.Stat(bakDirAbsPath); os.IsNotExist(err) {
+		fmt.Println("Directory does not exist")
+		err := os.Mkdir(bakDirAbsPath, os.ModePerm)
+		if err != nil {
+			fmt.Println("Could not create directory:", err)
+			return
+		}
+	}
+	fmt.Printf("%s created\n", bakDirAbsPath)
+	// Move original file to a bak directory
 	MoveFile(conf.Filepaths.Netinfo, bakFileAbsPath)
-	fmt.Println(bakFileAbsPath)
 	
+	// create an empty configuration file
 	f, err := os.Create(conf.Filepaths.Netinfo)
+	defer f.Close()
+	// if there will be an error revert back the old config file
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("Could not create file:", err)
+		MoveFile(bakFileAbsPath, conf.Filepaths.Netinfo)
 	}
 
-	defer f.Close()
-
-	err = t.Execute(f, conf)
+	// execute the template with the json data
+	err = tpl.Execute(f, conf)
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("Could not execute template:", err)
 	}
 }
 
 func MoveFile(src, dest string) {
 	err := os.Rename(src, dest)
 	if err != nil {
-		fmt.Println("Could not move file: ", err)
+		fmt.Println("Could not rename file: ", err)
 		return
 	}
-
-	fmt.Println(time.Now())
+	fmt.Printf("Moved %s to %s\n", src, dest)
 }
 
 func LoadConfig(file string) Config {
 	var config Config
 	configFile, err := ioutil.ReadFile(file)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("Could not load config.json:", err.Error())
 		os.Exit(1)
 	}
 
 	json.Unmarshal(configFile, &config)
+	fmt.Println("json file loaded")
 
 	return config
+}
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
